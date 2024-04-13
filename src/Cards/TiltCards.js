d78994a83b91af67a0d066c96826110e6164ab2a
Alexandru <alexandru.mucenic@kubisinteractive.com>
2024-04-13 22:57:27 +0300

    new titled package and fixing the onMouseLeave bug

--- src/Cards/TiltCards.js
+++ src/Cards/TiltCards.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { Tilt } from "react-tilt";
+import Tilt from "react-parallax-tilt";
 import "./style.scss";
 
 export default function TiltCard({ data }) {
   return (
-    <div class="tilt">
+    <div className="tilt">
       {data.map((card, index) => (
         <Tilt
           key={index}
+          glareEnable={true}
+          perspective={1000}
+          tiltMaxAngleX={40}
+          tiltMaxAngley={40}
+          glareMaxOpacity={0.3}
+          glareColor={"white"}
           className="tilt__container"
-          options={{ max: 45, scale: 1 }}
         >
           <div className="tilt__card">
             <div
